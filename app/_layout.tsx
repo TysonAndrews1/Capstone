387ea7b0a7fcb968dd0e8844a0f9e6b0c54386f3
TysonAndrews1 <tyson.andrews@edu.sait.ca>
2024-12-03 16:50:59 -0700

    Forgot Password Screen added

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -18,6 +18,8 @@ export default function RootLayout() {
       <Stack.Screen name="screens/EventList" options={{ title: 'Manage Events' }} /> {/* Navigate to screens/Events */}
       <Stack.Screen name="screens/calenderTest"  options={{ title: 'Test' }}/> {/* Navigate to screens/CalendarTest */}
       <Stack.Screen name="screens/EventEdit"  options={{ title: 'Edit Events' }}/> {/* Navigate to screens/CalendarTest */}
+      <Stack.Screen name="screens/forgotPassword"  options={{ title: 'Forgot Password', headerShown: false }}/> {/* Navigate to screens/CalendarTest */}
+      <Stack.Screen name="screens/ManagerScreen"  options={{ title: 'Manager Options' }}/> {/* Navigate to screens/CalendarTest */}
     </Stack>
   );
 }
