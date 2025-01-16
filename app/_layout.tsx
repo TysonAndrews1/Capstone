9974423d3b241650a89caa9ea0100bd76086efe9
maecison <130397579+maecison@users.noreply.github.com>
2025-01-16 11:36:25 -0700

    Navigation to EmployeeAccounts and changed header text.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout() {
       <Stack.Screen name="screens/EventList" options={{ title: 'Manage Events' }} /> {/* Navigate to screens/Events */}
       <Stack.Screen name="screens/calenderTest"  options={{ title: 'Test' }}/> {/* Navigate to screens/CalendarTest */}
       <Stack.Screen name="screens/EventEdit"  options={{ title: 'Edit Events' }}/> {/* Navigate to screens/CalendarTest */}
+      <Stack.Screen name="screens/EmployeeAccounts"  options={{ title: 'Manage Employee Accounts' }}/> {/* Navigate to screens/EmployeeAccounts */}
       <Stack.Screen name="screens/forgotPassword"  options={{ title: 'Forgot Password', headerShown: false }}/> {/* Navigate to screens/CalendarTest */}
       <Stack.Screen name="screens/ManagerScreen"  options={{ title: 'Manager Options' }}/> {/* Navigate to screens/CalendarTest */}
       <Stack.Screen name='screens/Login' options={{ headerShown: false}} /> {/* remove a header on the login screen */}
