b6804be9bed3537d72a67b85da06ad6234895a4b
Jooyoung Kim <141282000+teddy-1991@users.noreply.github.com>
2025-01-17 17:12:30 -0700

    added AddSifht screen, edited file name

---
+++ app/screens/manager/AddShift.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const AddShift = () => {
+  return (
+    <View>
+      <Text>AddShift</Text>
+    </View>
+  );
+};
+
+export default AddShift;
+
+const styles = StyleSheet.create({});
