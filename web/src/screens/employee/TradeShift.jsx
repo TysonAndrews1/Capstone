aefc02a3ab642a2466d0f9d856d02993388f640e
Michellepotran <113163108+Michellepotran@users.noreply.github.com>
2025-01-21 23:11:57 -0700

    Added dropdown menu for the logged in user's shift

--- web/src/screens/employee/TradeShift.jsx
+++ web/src/screens/employee/TradeShift.jsx
@@ -42,10 +42,22 @@ export default function TradeShift() {
     return (
       <div className="p-4">
 
+        {/* Current Logged in User */}
         <div className="mb-4">
-        <h1 className="text-lg font-bold mb-4">Select Coworker</h1>
+          <h1 className="text-lg font-bold mb-4">Current User</h1>
+        </div>
+
+        {/* Dropdown to select the current logged in user's shift */}
+        <div className="mb-4">
+          <h1 className="text-lg font-bold mb-4">Your Shifts</h1>
+        <select className="w-full p-2 border border-gray-300 rounded">
+          <option value="">-- Select Shift --</option>
+        </select>
+        </div>
 
         {/* Dropdown to select coworker */}
+        <div className="mb-4">
+          <h1 className="text-lg font-bold mb-4">Select Coworker</h1>
         <select className="w-full p-2 border border-gray-300 rounded">
           <option value="">-- Select Coworker --</option>
           {accounts.map((account) => (
@@ -56,9 +68,9 @@ export default function TradeShift() {
         </select>
         </div>
 
+        {/* Selected coworker's shift */}
         <div className="mb-4">
           <h1 className="text-lg font-bold mb-4">Available Shifts</h1>
-        {/* Selected coworker's shift */}
         <select className="w-full p-2 border border-gray-300 rounded">
           <option value="">-- Select Shift --</option>
         </select>
