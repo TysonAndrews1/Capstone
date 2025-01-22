f47f32f72c658065dfcc4aaf9bb5c88aa3398972
Michellepotran <113163108+Michellepotran@users.noreply.github.com>
2025-01-21 17:44:04 -0700

    Setting up the Trade Request page
    Still trying to figure out how to fetch from the database

--- web/src/screens/Home.jsx
+++ web/src/screens/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import Overlay from "../components/Overlay";
 import Roster from "../components/Roster";
-import CalendarComponent from "../components/Calender";
+
 
 //Created By Tyson
 //The Home Page dedicated to future navigation of the page 
@@ -15,7 +15,6 @@ return (
     <p>
 This is the Home
 </p>
-<CalendarComponent/>
 <Overlay child={<Roster/>} headerTitle={"View Roster"} ButtonTitle={"View Roster"} buttonPlacement={"top-[75vh] left-[55vw]"}/>
 
 </div>)
--- web/src/screens/employee/TradeShift.jsx
+++ web/src/screens/employee/TradeShift.jsx
@@ -1,13 +1,58 @@
 import React from 'react';
-import MainLayout from '../../layouts/MainLayout';
+import { use } from 'react';
+import { useState, useEffect } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
 
-function TradeShift() {
+
+/**
+ * Personal Notes:
+ * We need to create endpoints to get the logged-in employee data which will return the currently logged-in employee's details and shifts
+ * We need to create an endpoint to get shifts for the chosen employee which will return the employee id and their scheduled shifts
+ * 
+ */
+
+export default function TradeShift() {
+  const [accounts, setAccounts] = useState([]);
+  const BASE_URL = 'http://localhost:8080/api/accounts';
+
+  // Function to fetch all banquet accounts from the backend 
+  const fetchAccounts = async () => {
+    try {
+      const response = await fetch(`${BASE_URL}/accounts`);
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const data = await response.json();
+      setAccounts(data); // Update the state with the fetched accounts
+      console.log(data);
+    } catch (err) {
+      console.error('Error fetching accounts:', err);
+    }
+  };
+
+  // example date to see if working 
+  const mockAccounts = [
+    {accountId: 1, firstName: 'John'},
+    {accountId: 2, firstName: 'Jane'},
+  ]
+
+    useEffect(() => { 
+      setAccounts(mockAccounts);
+    }, []);
+ 
     return (
-        <div>
-            <h1>This is the trading shift page </h1>
-            
-        </div>
-    )
+      <div className="p-4">
+        <h1 className="text-lg font-bold mb-4">Select Coworker</h1>
+        <select className="w-full p-2 border border-gray-300 rounded">
+          <option value="">-- Select Coworker --</option>
+          {accounts.map((accounts) => (
+            <option key={accounts.accountId} value={accounts.accountId}>
+              {accounts.firstName}
+            </option>
+          ))}
+        </select>
+      </div>
+    );
+    
 }
 
-export default TradeShift;
