dd49d02a24515a149b81a3febf51828b398581e2
TysonAndrews1 <tyson.andrews@edu.sait.ca>
2025-01-23 11:32:18 -0700

    Some Overlay Fixing

--- web/src/screens/Home.jsx
+++ web/src/screens/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useState} from "react";
 import { useNavigate } from "react-router-dom";
 import Overlay from "../components/Overlay";
 import Roster from "../components/Roster";
@@ -8,14 +8,14 @@ import Roster from "../components/Roster";
 //The Home Page dedicated to future navigation of the page 
 
 export default function Home(){
-
+            const [activeOverlay, setActiveOverlay] = useState(null)
 return (
 
 <div>
     <p>
 This is the Home
 </p>
-<Overlay child={<Roster/>} headerTitle={"View Roster"} ButtonTitle={"View Roster"} buttonPlacement={"top-[75vh] left-[55vw]"}/>
+<Overlay child={<Roster/>} headerTitle={"View Roster"} ButtonTitle={"View Roster"} buttonPlacement={"top-[75vh] left-[55vw]"} isActive={activeOverlay === "View Roster"} onToggle={setActiveOverlay}/>
 
 </div>)
 }
