077fb80c24e017d40758234345141dc238270dfb
lyaaronSAIT <130510117+lyaaronSAIT@users.noreply.github.com>
2025-03-03 00:49:39 -0700

    Added more request

--- Backend/test.sql
+++ Backend/test.sql
@@ -177,7 +177,7 @@ CREATE TABLE `employee_requests` (
   `status` ENUM('PENDING', 'APPROVED', 'DECLINED') NOT NULL DEFAULT 'PENDING',
   PRIMARY KEY (`request_id`),
   FOREIGN KEY (`account_id`) REFERENCES `banquet_accounts` (`account_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 -- Locks the employee_requests table from other connections from modifying it during the insert operation
@@ -195,7 +195,17 @@ INSERT INTO `employee_requests` VALUES
 (8, 6, 'Time Off', '2025-02-21 10:00:00', '2025-03-01 08:00:00', '2025-03-02 23:00:00', 'Need to take my car to the autobody shop', 'PENDING'),
 (9, 7, 'Time Off', '2025-01-29 10:30:00', '2025-05-01 08:00:00', '2025-05-31 23:00:00', 'Going to Europe for a month', 'APPROVED'),
 (10, 7, 'Availability Change', '2025-03-02 10:00:00', NULL, NULL, 'Can only work weekends once school starts', 'PENDING'),
-(11, 3, 'Availability Change', '2025-01-01 8:00:00', NULL, NULL, 'Going back to my full job, will be back for the christmas season', 'APPROVED');
+(11, 3, 'Availability Change', '2025-01-01 8:00:00', NULL, NULL, 'Going back to my full job, will be back for the christmas season', 'APPROVED'),
+(12, 2, 'Time Off', '2024-10-11 09:00:00', '2024-12-01 09:00:00', '2024-12-20 17:00:00', 'Final Exams', 'APPROVED'),
+(13, 2, 'Time Off', '2025-03-02 11:00:00', '2025-03-03 08:00:00', '2024-03-04 23:00:00', 'Sorry for short notice but my friends want to go snowboarding', 'DECLINED'),
+(14, 2, 'Time Off', '2025-03-06 10:00:00', '2025-03-22 08:00:00', '2024-03-23 23:00:00', 'Snowboarding Trip', 'APPROVED'),
+(15, 2, 'Time Off', '2025-03-12 08:00:00', '2025-04-15 08:00:00', '2024-04-20 23:00:00', 'Finals week! need to study', 'PENDING'),
+(16, 6, 'Time Off', '2025-03-01 10:00:00', '2025-04-01 08:00:00', '2025-04-07 23:00:00', 'Wisdom tooth surgery and recovery', 'APPROVED'),
+(17, 6, 'Time Off', '2025-02-03 09:00:00', '2025-02-05 08:00:00', '2025-02-05 23:00:00', 'Friends want to go to banff', 'DECLINED'),
+(18, 6, 'Time Off', '2025-01-02 10:00:00', '2025-01-25 08:00:00', '2025-01-26 23:00:00', 'Booking off for my birthday', 'APPROVED'),
+(19, 6, 'Time Off', '2025-03-02 10:00:00', '2025-03-03 08:15:00', '2025-03-04 23:00:00', 'Snowboarding trip with peter! Decline if its too short of a notice', 'DECLINED'),
+(20, 6, 'Time Off', '2025-03-20 10:00:00', '2025-06-01 08:00:00', '2025-06-31 23:00:00', 'Found a summber job, might be gone in june', 'PENDING');
+
 /*!40000 ALTER TABLE `employee_requests` ENABLE KEYS */;
 
 -- Unlocks the employee_requests table
